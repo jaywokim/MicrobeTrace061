d784de804f6744eeee217d5cb9c21621a46b59f4
czk2 <czk2@cdc.gov>
2020-02-03 13:07:47 -0500

    wiki

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    preset: "jest-puppeteer",
+    testTimeout: 30000,
+    verbose: true
+    // preset: "jest-puppeteer",
+    // testMatch: [
+    //   "**/test/**/*.test.js",
+    //   "/test/**/*.test.js",
+    //   "test/**/*.test.js"
+    // ],
+    // verbose: true
+}
---
+++ test/e2e/test.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const { toMatchImageSnapshot } = require('jest-image-snapshot')
+expect.extend({ toMatchImageSnapshot });
+
+const timeout = process.env.SLOWMO ? 30000 : 10000;
+jest.setTimeout(30000);
+
+describe('viz reg test', () => {
+    test('random node graph', async () => {
+        
+        // const title = await page.title();
+        // expect(title).toBe('E2E Puppeteer Testing');
+
+        await page.goto('http://localhost:5000/')
+        // await page.setViewport({ width: 1053, height: 809 })
+        await page.waitFor(1000);
+        await page.click("#acceptAgreement")
+        await page.waitFor(1000);
+        await page.click("span.oi")
+        await page.waitFor(1000);
+        await page.click("#experimental-tab")
+        await page.waitFor(1000);
+        await page.click("#generate-sequences")
+        await page.waitFor(500);
+        await page.click("#launch")
+        await page.waitFor(2000);
+        global.__basedir = __dirname;
+        // const image = fs.readFileSync('./__image_snapshots__/Exp.png')
+        // expect(image).toMatchImageSnapshot();
+        const screen = await page.screenshot();
+        expect(screen).toMatchImageSnapshot();
+        
+    }, timeout);
+});
